cb257e249700b42f0edc0161f53819a27b7f6ee6
Igahsamuel <igahsamuela2@gmail.com>
2023-09-16 09:14:32 +0100

    added new change

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -3,15 +3,15 @@ function TaskItem({ task, onDelete }) {
   return (
     <>
       <div className="bg-[#fff] p-[10px] my-[10px] border-1 border-[#ccc] rounded shadow-[0_2px_4px_rgba-(0,0,0,0.1)]">
-        <div className="flex justify-center flex-col items-center flex-1">
+        <div className="flex justify-center md:justify-evenly md:flex-row flex-col items-center flex-1">
           <span>{task.name}</span>
           <span>
-            <span className="md:block">Due Date:</span>
+            <span>Due Date:</span>
             {task.dueDate}
           </span>
         </div>
         <div className="my-[20px]">
-          <span>{task.taskContent}</span>
+          <span className="flex flex-wrap px-4">{task.taskContent}</span>
         </div>
         <Button onDelete={onDelete}>Delete</Button>
       </div>
